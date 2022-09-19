267e601dcf600337f757f7a1d25f73bfc8d847c4
RAVIDHOTE <ravidhote6593@gmail.com>
2022-09-19 10:39:55 +0530

    2nd commit

---
+++ Git_Pro2/src/methods/Static_method.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class Static_method {
+	
+	public static void run1() {
+		System.out.println("Hello world");
+	}
+	
+	public static void main(String [] args) {
+		run1();
+	}
+
+}
